eea57f0bbb665b5d381979d15781cf72cafa87d0
Ben Vinnick <benvinnick@gmail.com>
2022-05-25 14:01:11 -0700

    fixed types in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-/*
-html:
-- dropdown pick team
-- display macros (click to copy)
+// ----- TYPES -----
+interface Player {
+  jerseyNumber: string;
+  person: {
+    fullName: string;
+  }
+}
 
-ts:
-- api req
-- organize data
-- build string
-*/
+interface Team {
+  name: string;
+  players: Array<Player>;
+}
 
 enum Sort {
   Name,
@@ -16,30 +18,30 @@ enum Sort {
 
 // ----- ----- -----
 const url = "https://statsapi.web.nhl.com/api/v1/teams";
+
 // specifies sort type (first name by default)
 let sort_type: Sort = Sort.Name;
 
 // needs to be async?
 async function main() {
   try {
-    let team_data = await get_team_data((<HTMLInputElement>document.getElementById("chosen-team")).value);
+    let team_data: Team = await get_team_data((<HTMLInputElement>document.getElementById("chosen-team")).value);
 
     sort_players(team_data.players, parseInt((<HTMLInputElement>document.getElementById("order")).value));
-
-
 
     let macro_string: string = build_team_macros(team_data, (<HTMLInputElement>document.getElementById("code")).value);
 
     (<HTMLInputElement>document.getElementById("display-box")).value = macro_string;
   } catch (error: unknown) {
-    console.log(error);
-    // @ts-ignore
-    alert(error.message);
+    console.error(error);
+    if (error instanceof Error) {
+      alert(error.message);
+    }
   }
 }
 
 // gets player data for specified NHL team
-async function get_team_data(team: any) {
+async function get_team_data(team: string): Promise<Team> {
   const teams_response = await fetch(url);
   const teams_parse_response = await teams_response.json();
 
@@ -58,43 +60,43 @@ async function get_team_data(team: any) {
   const players_parse_response = await players_response.json();
 
   return {
-    team,
-    players: players_parse_response.teams[0].roster.roster
+    name: team,
+    players : players_parse_response.teams[0].roster.roster
   }
 }
 
 // builds macro string for given team based on given code
-function build_team_macros(team_data: any, code: any): string {
+function build_team_macros(team_data: Team, code: string | undefined): string {
   if (!code) {
-    code = team_data.team.slice(0, 1);
+    code = <string>team_data.name.slice(0, 1);
     code = code.toLowerCase();
   }
 
   let output_string: string = "";
 
   // team name macro
-  output_string += `${code}\tthe ${team_data.team}\n\n`;
+  output_string += `${code}\tthe ${team_data.name}\n\n`;
 
   // build player macros
-  team_data.players.forEach((player: any) => {
+  team_data.players.forEach((player: Player) => {
     let name: string = player.person.fullName;
-    let number: number = player.jerseyNumber;
+    let number: number = parseInt(player.jerseyNumber);
     // \v, \vv, \vvv
     output_string +=
-      `\n\n${code}${number}\t${name} #${number} of the ${team_data.team}\n${code}${code}${number}\t${name}\n${code}${code}${code}${number}\t${name} #${number}`;
+      `\n\n${code}${number}\t${name} #${number} of the ${team_data.name}\n${code}${code}${number}\t${name}\n${code}${code}${code}${number}\t${name} #${number}`;
   });
 
   return output_string;
 }
 
-function sort_players(players: any, sort_by: Sort) {
-  let compare_fn: Function;
+function sort_players(players: Array<Player>, sort_by: Sort) {
+  let compare_fn: (player1: Player, player2: Player) => number;
   if (sort_by == Sort.Name) {
-    compare_fn = (player1: any, player2: any) => {
+    compare_fn = (player1: Player, player2: Player) => {
       return player1.person.fullName.split(" ")[1].localeCompare(player2.person.fullName.split(" ")[1]);
     }
   } else {
-    compare_fn = (player1: any, player2: any) => {
+    compare_fn = (player1: Player, player2: Player) => {
       if (parseInt(player1.jerseyNumber) < parseInt(player2.jerseyNumber)) return -1;
       else return 1;
     }
@@ -117,7 +119,6 @@ async function build_input_list() {
   const teams_parse_response = await teams_response.json();
 
   teams_parse_response.teams.forEach((team: any) => {
-
     let team_elem = document.createElement("option");
     team_elem.value = team.name;
     select_elem.appendChild(team_elem);
